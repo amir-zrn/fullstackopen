97254fa12f8b71ec18f22e6a3f2c322f42939fad
Amir Zareian <amir.zareiyan01@gmail.com>
2022-03-05 19:08:07 +0330

    part 1.7 commit

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,8 +6,13 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let all = bad + good + neutral;
+  let avg = ((all !== 0)) ? (good - bad) / all : 0;
+  let perc = ( (all !== 0)) ? (good / all) * 100 : 0
+  console.log(all !== 0)
   return (
     <div>
+
       <h4 className="title">give feedback</h4>
       <Button onClick={() => setGood(good+1)} text="good" />
       <Button onClick={() => setNeutral(neutral+1)} text="neutral" />
@@ -16,6 +21,9 @@ const App = () => {
       <Values text="good" value={good} />
       <Values text="neutral" value={neutral} />
       <Values text="bad" value={bad} />
+      <Values text="all" value={all} />
+      <Values text="average" value={avg} />
+      <Values text="Percentage" value={perc + `%`} />
     </div>
   )
 }
