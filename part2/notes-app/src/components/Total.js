b9840934d011edd791863298afe957c872988f0d
Amir Zareian <amir.zareiyan01@gmail.com>
2022-03-10 20:56:20 +0330

    part 2.4 changes

--- part2/notes-app/src/components/Total.js
+++ part2/notes-app/src/components/Total.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Total = ({ courseExcs }) => { 
-    var total = courseExcs.map(courseExc => courseExc.exercises)
-    const sum = total.reduce((partialSum, a) => partialSum + a, 0);
-    return (
-        <div>
-            <b>Total exercises of: {sum}</b>
-            
-        </div>
-    )
-}
- 
-export default Total
+const Total = ({ courseExcs }) => {
+	var total = courseExcs.map((courseExc) => courseExc.exercises);
+	const sum = total.reduce((partialSum, a) => partialSum + a, 0);
+	return (
+		<div>
+			<b>Total exercises of: {sum}</b>
+		</div>
+	);
+};
+
+export default Total;
