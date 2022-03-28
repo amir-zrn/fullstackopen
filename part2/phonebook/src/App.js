b5cc82270035e4d670f2aef19bf23b9eadc549b5
Amir Zareian <amir.zareiyan01@gmail.com>
2022-03-28 20:50:20 +0430

    part 2.11 changes

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Results from './components/Results'
+import axios from "axios"
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
   const [filterResults, setFilterResults] = useState(persons)
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setFilterResults(response.data)
+      })
+  }, [])
+  
+console.log(persons)
   const changeNameValue = (event) => {
     setNewName(event.target.value)
-    console.log(event.target.value);
   }
   
   const changeNumValue = (event) => {
@@ -52,7 +57,6 @@ const App = () => {
         return person.name.includes(el)
       }
     })
-    console.log(filteredResult);
     if (el !== "") {
       setFilterResults(filteredResult)
     }else{
